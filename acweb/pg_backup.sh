7a2e627a104fa46be750aa70e181623592a0addb
Landon Fowler <landon.fowler@linuxacademy.com>
2020-02-12 12:53:21 -0500

    Create pg_backup.sh

---
+++ acweb/pg_backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Backup directory and name suffix
+BACKUP_DIR=/var/lib/pgsql/12/data
+FILE_SUFFIX=_pg_backupl.tar
+
+# Name file using the date and supplied suffix
+FILE=`date +"%Y%m%d%H%M"`${FILE_SUFFIX}
+
+# Combine the backup directory and file name
+OUTPUT_FILE=${BACKUP_DIR}/${FILE}
+
+# Execute the backup as a tar
+pg_dump -F t acweb > ${OUTPUT_FILE}
