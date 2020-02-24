4bfc291701fa69bd735116d6136217ccc298f1af
Landon Fowler <landon.fowler@linuxacademy.com>
2020-02-24 13:27:39 -0500

    Load generation script

--- acweb/loadgen.sh
+++ acweb/loadgen.sh
@@ -5,11 +5,23 @@
 # Script to gennerate loade on PostgreSQL server
 ################################################
 
+
 #Iterator
 i=1
 
-#Infinitely loop and insert rows to sales.customer
+####
+# Function to insert rows into sales.customer and select large amounts from sales.products
+run_sql () {
+
+  psql -d acweb -c "INSERT INTO sales.customer (first_name, last_name) VALUES ('Delmar', 'Database');"
+  psql -d acweb -c "SELECT * FROM sales.products;" > /dev/null
+}
+####
+
+
+
+#Infinitely loop and call our function
 while [ $i -lt 2 ]
 do
-  psql -d acweb -c "INSERT INTO sales.customer (first_name, last_name) VALUES ('Delmar', 'Database');"
+  run_sql
 done
